6a207ba2978052f8bd9f123b60550b65bb2d15c5
Veronika Fedorova <fedorovavv03@gmail.com>
2024-08-14 18:23:06 +0300

    Finish the pricing secection

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -1,3 +1,4 @@
+import { ScrollParallax } from "react-just-parallax";
 import { smallSphere, stars } from "../assets";
 import Heading from "./Heading";
 import PricingList from "./PricingList";
@@ -16,15 +17,17 @@ const Pricing = () => {
             height={255}
             alt="Sphere"
           />
-          <div className="absolute top-1/2 left-1/2 w-[60rem] -translate-x-1/2 -translate-y-1/2 pointer-events-none">
-            <img
-              src={stars}
-              className="w-full"
-              width={950}
-              height={400}
-              alt="Stars"
-            />
-          </div>
+          <ScrollParallax isAbsolutelyPositioned>
+            <div className="absolute top-1/2 left-1/2 w-[60rem] -translate-x-1/2 -translate-y-1/2 pointer-events-none">
+              <img
+                src={stars}
+                className="w-full"
+                width={950}
+                height={400}
+                alt="Stars"
+              />
+            </div>
+          </ScrollParallax>
         </div>
         <Heading
           tag="Get started with Brainwave"
@@ -34,6 +37,14 @@ const Pricing = () => {
           <PricingList />
           <LeftLine />
           <RightLine />
+        </div>
+        <div className="flex justify-center mt-10">
+          <a
+            className="text-xs font-code font-cold tracking-wider hover:text-color-6 cursor-pointer uppercase border-b"
+            href="/pricing"
+          >
+            See the full details
+          </a>
         </div>
       </div>
     </Section>
