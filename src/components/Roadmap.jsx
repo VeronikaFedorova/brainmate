322528436edfcdc076c70c123f0bc379d4bac8d3
Veronika Fedorova <fedorovavv03@gmail.com>
2024-08-15 15:05:36 +0300

    Add Footer component

--- src/components/Roadmap.jsx
+++ src/components/Roadmap.jsx
@@ -1,5 +1,6 @@
 import { check2, grid, loading1 } from "../assets";
 import { roadmap } from "../constants";
+import Button from "./Button";
 import Heading from "./Heading";
 import Section from "./Section";
 import Tagline from "./Tagline";
@@ -62,6 +63,7 @@ const Roadmap = () => {
             );
           })}
         </div>
+        <Button className='relative left-[50%] translate-x-[-50%] mt-8' children='Our roadmap' />
       </div>
     </Section>
   );
